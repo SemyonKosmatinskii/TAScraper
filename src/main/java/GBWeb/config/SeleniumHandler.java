f3802e8a101b654e50dca9c1731b2181a4d348f8
KSemyon <kosmas92@yandex.ru>
2021-09-29 13:55:00 +0400

    commit 1

--- src/main/java/GBWeb/config/SeleniumHandler.java
+++ src/main/java/GBWeb/config/SeleniumHandler.java
@@ -1,13 +1,20 @@
 package GBWeb.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 
 @Slf4j
 public class SeleniumHandler {
 
+    private static final int WAIT_TIME_MAX = 6;
     private WebDriver driver;
+    private Wait<WebDriver> wait;
 
     public boolean start(boolean headlessMode) {
         try {
@@ -27,6 +34,15 @@ public class SeleniumHandler {
             return false;
         }
         return true;
+    }
+
+    public WebElement getElement(String xpath) {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+    }
+
+    public void click(WebElement webElement) {
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView(true);arguments[0].click()", webElement);
     }
 
     public void stop() {
